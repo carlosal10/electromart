4b43b138651f1dc3526fb007ebfb1e21a5825fb8
Owuor Timon Odhiambo <infotechhaven6@gmail.com>
2025-07-03 13:56:39 +0300

    update

---
+++ electromart/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // ✅ No BrowserRouter here
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import AddProduct from './pages/AddProduct';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import AdminProductList from './pages/AdminProductList';
+
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/add-product" element={<AddProduct />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/admin/products" element={<AdminProductList />} />
+    </Routes>
+    <Footer />
+  </>
+);
+
+export default App;
---
+++ electromart/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <h1>Welcome to ElectroMart Home</h1>;
+};
+
+export default Home;
