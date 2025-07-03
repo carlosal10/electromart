763b44f4b60b94cebbaeb97ab2ad91a15e3963d5
Owuor Timon Odhiambo <infotechhaven6@gmail.com>
2025-07-03 14:16:26 +0300

    update

--- electromart/src/pages/Home.jsx
+++ electromart/src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // ✅ import Link
+import './styles.css';
 
 const Home = () => {
-  return <h1>Welcome to ElectroMart Home</h1>;
+  return (
+   <> <section className="hero">
+      <div className="hero-content">
+        <h1>Upgrade Your Tech</h1>
+        <p>Best deals on the latest electronics</p>
+        <Link to="/shop" className="cta-btn">Shop Now</Link>
+      </div>
+    </section>
+
+    <section className="products">
+    <h2>Featured Products</h2>
+    <div className="product-grid">
+      <div className="product-card">Product 1</div>
+      <div className="product-card">Product 2</div>
+      <div className="product-card">Product 3</div>
+    </div>
+  </section>
+  </>
+  );
 };
 
 export default Home;
