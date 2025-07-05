881d88c0397ac022a99582af4522af001a1757c1
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-05 15:32:36 +0300

    Update App.jsx

--- electromart/src/App.jsx
+++ electromart/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // ✅ No BrowserRouter here
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,21 +9,24 @@ import Login from './pages/Login';
 import AdminProductList from './pages/AdminProductList';
 import EditProduct from './pages/EditProduct';
 import CartPage from './pages/CartPage';
+import { CartProvider } from './context/CartContext';
 
 const App = () => (
-  <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/add-product" element={<AddProduct />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/admin/products" element={<AdminProductList />} />
-      <Route path="/admin/edit-product/:id" element={<EditProduct />} />
-      <Route path="/cart" element={<CartPage />} />
-    </Routes>
-    <Footer />
-  </>
+  <CartProvider> {/* ✅ Enable Cart Context Globally */}
+    <Router> {/* ✅ Required for Routes to work */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin/products" element={<AdminProductList />} />
+        <Route path="/admin/edit-product/:id" element={<EditProduct />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </CartProvider>
 );
 
 export default App;
