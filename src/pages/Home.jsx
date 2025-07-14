b239e92b6929fd5b30714744dd44f06d0bd3149e
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-14 19:52:59 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
+import Header from './components/Navbar';
 import Hero from './components/Hero';
 import './App.css';
 
