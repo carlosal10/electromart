ca8c175a3f11b29965e47a59c5c39f32142bf2cb
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-16 01:06:03 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const App = () => (
         <Route path="/signup" element={<Signup />} />
         <Route path="/my-orders" element={<MyOrders />} />
         <Route path="/product/:id" element={<ProductDetail />} /> {/* ✅ Correct location */}
-         <Route path="/products/:id" element={<ProductDetail />} /> {/* ✅ Correct location */}
+         
 
 
         {/* Admin Routes */}
