25946bfee577877149b3dcf0287ff3053f41ec37
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-16 12:49:45 +0000

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FiShoppingCart } from 'react-icons/fi';
 import Hero from '../components/Hero';
 import PopularProducts from '../components/PopularProducts';
 import './App.css';
