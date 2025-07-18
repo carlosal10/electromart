2e2d0510ff8aaca30da68ef58a095b99496e1bc5
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-18 21:54:32 +0000

    update

--- src/components/CenterCarousel.jsx
+++ src/components/CenterCarousel.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import axios from 'axios';
 import DynamicProductCard from './ProductCard';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ShowcaseCenter = () => {
   const [products, setProducts] = useState([]);
